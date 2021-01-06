46dfccd56749e536d1b3bd7f16d1303676a6b08e
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2021-01-06 17:06:25 +0800

    feat(analysis): add dynamic time range selection for the all-year chart
    work on #8

---
+++ js/analysis.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#time-slider").bind("valuesChanged", function(e, data) {
+        let start = data.values.min;
+        let end = data.values.max;
+        update_all_year_plot(start, end)
+    });
+});
