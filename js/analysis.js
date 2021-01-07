72e7b761b15c07c28dd59b735fe78a4041b22255
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2021-01-07 20:39:33 +0800

    feat(analysis): add dynamic time range selection for the stack plot

--- js/analysis.js
+++ js/analysis.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $("#time-slider").bind("valuesChanged", function(e, data) {
         let start = data.values.min;
         let end = data.values.max;
-        update_all_year_plot(start, end)
-        plot_typhoon(start, end)
+        update_all_year_plot(start, end);
+        plot_typhoon(start, end);
+        plot_stack(start, end);
     });
 });
