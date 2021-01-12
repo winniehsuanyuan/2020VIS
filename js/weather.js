e94b775316081eaef65d072155c9f4d9d25e4f4b
winniehsuanyuan <starlightwinnie@gmail.com>
2021-01-12 20:50:19 +0800

    weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,25 @@
   $(document).ready(function() {
-    $('select[name="selectCropYear"]').on('change', function(e) {
+    $('#crop').on('change', function(e) {
       var crop = $('#crop').val();
       var year = $('#year').val();
       var weather = $('#weather').val();
       plot_pcp(crop, year, weather);
       plot_calendar(crop, year);
     });
-    $('select[name="selectWeather"]').on('change', function(e) {
+    $('#year').on('change', function(e) {
+      var crop = $('#crop').val();
+      var year = $('#year').val();
+      var weather = $('#weather').val();
+      plot_pcp(crop, year, weather);
+      plot_calendar(crop, year);
+    });
+    $('#weather').on('change', function(e) {
       var crop = $('#crop').val();
       var year = $('#year').val();
       var weather = $('#weather').val();
       plot_pcp(crop, year, weather);
     });
+
 });
 
 
