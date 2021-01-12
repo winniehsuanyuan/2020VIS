ae8525e90726370cfb06da1d7bb4ad83970a0aea
winniehsuanyuan <starlightwinnie@gmail.com>
2021-01-12 22:23:26 +0800

    hover stackArea

--- component/boxPlot_stackArea/stackArea.js
+++ component/boxPlot_stackArea/stackArea.js
@@ -4,7 +4,7 @@ const height = +svg.attr('height');
 const margin = { top: 10, right: 80, bottom: 30, left: 80 }; //left150
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
-const colors = ['rgba(228,27,19,1)', 'rgba(241,134,14,1)', 'rgba(253,200,0,1)', 'rgba(152,198,70,1)', 'rgba(27,165,72,1)', 'rgba(0,156,166,1)', 'rgba(0,163,226,1)', 'rgba(0,87,184,1)', 'rgba(104,91,199,1)', 'rgba(180,0,158,1)', 'rgb(184,161,207)', 'rgb(230, 138, 184)', 'rgb(255, 192, 203)'];
+const colors = ['rgba(228,27,19,1)', 'rgba(241,134,14,1)', 'rgba(253,200,0,1)', 'rgba(152,198,70,1)', 'rgba(27,165,72,1)', 'rgba(0,156,166,1)', 'rgba(0,163,226,1)', 'rgba(0,87,184,1)', 'rgba(104,91,199,1)', 'rgba(180,0,158,1)', 'rgba(184,161,207,1)', 'rgba(230, 138, 184,1)', 'rgba(255, 192, 203,1)'];
 var start_time = new Date(2012, 0, 1);
 var end_time = new Date(2019, 11, 31);
 
@@ -15,7 +15,6 @@ function plot_stack(crop, start, end, norm) {
     let g = svg.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
     var n;
-    console.log(norm);
     if(norm) n = 'norm/';
     else n = 'unnorm/';
     // Parse the Data
@@ -53,6 +52,7 @@ function plot_stack(crop, start, end, norm) {
         var y = d3.scaleLinear()
             .domain([0, d3.max(stackedData[markets.length - 1], d => d[1])])
             .range([innerHeight, 0]);
+        if(!norm) y.nice();
 
         g.append('g')
             .call(d3.axisLeft(y));
@@ -62,31 +62,60 @@ function plot_stack(crop, start, end, norm) {
             .domain(markets)
             .range(colors);
 
-        // Show the areas
-        g.selectAll('layers')
-            .data(stackedData)
-            .enter()
-            .append('path')
+        const hoverArea = function(h) {
+            d3.selectAll('.layers','.legendCircle')
+            .style('fill', d => color(d.key).replace('1)', '0.1'))
+            .attr('stroke', 'rgba(0,0,0,0.1)');
+            d3.selectAll('.legendText')
+            .style('opacity', 0.3);
+            d3.selectAll('.layers.'+h.key, '.legendCircle.'+h.key)
+            .style('fill', color(h.key))
+            .attr('stroke', 'rgba(0,0,0,1)')
+            d3.selectAll('.legendText.'+h.key)
+            .style('opacity', 0.85);
+        }
+        const noHoverArea = function(h) {
+            d3.selectAll('.layers','.legendCircle')
             .style('fill', d => color(d.key))
-            .attr('stroke', 'rgb(0,0,0)')
+            .attr('stroke', 'rgba(0,0,0,1)');
+            d3.selectAll('.legendText')
+            .style('opacity', 0.85);
+        }
+        // Show the areas
+        g.selectAll('.layers')
+            .data(stackedData).enter()
+            .append('path')
+            .attr('class', d => 'layers '+ d.key)
+            .style('fill', d => color(d.key))
+            .attr('stroke', 'rgba(0,0,0,1)')
             .attr('stroke-width', 0.05)
             .attr('d', d3.area()
                 .x(function(d, i) { return x(d.data.DateTime); })
                 .y0(function(d) { return y(d[0]); })
                 .y1(function(d) { return y(d[1]); })
-            )
+            ).on('mouseover', hoverArea)
+            .on('mouseleave', noHoverArea);
 
+        var pos = d3.scaleBand()
+            .domain(markets)
+            .range([innerHeight, 0]);
         //legend
         const legend = g.append('g')
             .attr('transform', `translate(${innerWidth+30}, 20)`);
-        let i = 0;
-        markets.reverse().forEach(m => {
-            legend.append('circle').attr('cx', 0).attr('cy', i * 20).attr('r', 5)
-                .style('fill', color(m));
-            legend.append('text').attr('x', 15).attr('y', i * 20).attr('class', 'legend')
-                .style('alignment-baseline', 'middle')
-                .text(m);
-            i++;
-        });
+        const l = legend.selectAll('.legend')
+            .data(stackedData).enter();
+        l.append('circle')
+            .attr('class', d => 'legendCircle '+ d.key)
+            .attr('cx', 0)
+            .attr('cy', d => pos(d.key))
+            .attr('r', 5)
+            .style('fill', d => color(d.key));
+        l.append('text')
+            .attr('x', 15).attr('y',  d => pos(d.key))
+            .attr('class', d => 'legendText '+ d.key)
+            .style('alignment-baseline', 'middle')
+            .text(d => d.key)
+            .on('mouseover', hoverArea)
+            .on('mouseleave', noHoverArea);
     });
 }
